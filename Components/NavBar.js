cabc79b3d3f04f0836ac8647ffc61e00fa159ca1
muhammadsemeer <muhammadsemeerali@gmail.com>
2022-04-18 15:45:30 +0530

    links

--- Components/NavBar.js
+++ Components/NavBar.js
@@ -1,12 +1,14 @@
+import Link from "next/link";
+
 const NavBar = () => {
   return (
     <nav>
       <div className="logo">
         <h1>Test List</h1>
       </div>
-      <a>Home</a>
-      <a>About</a>
-      <a>Test</a>
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/test">Test</Link>
     </nav>
   );
 };
