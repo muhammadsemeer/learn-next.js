90f789fdbf4c4a4648df6517b9a84902cf38209b
muhammadsemeer <muhammadsemeerali@gmail.com>
2022-04-18 15:14:19 +0530

    routing

---
+++ pages/test/index.js
@@ -0,0 +1,9 @@
+const Index = () => {
+    return ( 
+        <div>
+            <h1>All Test</h1>
+        </div>
+     );
+}
+ 
+export default Index;
---
+++ pages/test/sample.js
@@ -0,0 +1,9 @@
+const Sample = () => {
+  return (
+    <div>
+      <h1>Sample Test</h1>
+    </div>
+  );
+};
+
+export default Sample;
